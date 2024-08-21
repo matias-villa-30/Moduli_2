e968073e993fd3f69f131195bb3c2c1a8974c549
matias-villa-30 <matias.villalemes@metropolia.fi>
2024-08-21 09:18:57 +0300

    Update 4_numerot.py

--- 4_numerot.py
+++ 4_numerot.py
@@ -2,7 +2,7 @@ numero_1 = int(input("Kirjoita ensimäinen kokonaisluku: "))
 numero_2 = int(input("Kirjoita toinen kokonaisluku: "))
 numero_3 = int(input("Kirjoita kolmas kokonaisluku: "))
 
-suma = numero_1 + numero_2 + numero_3
-producto = numero_1 * numero_2 * numero_3
-promedio = suma / 3
-print(f"Keskiarvo on: {promedio} \nSumma on {suma} \nTulos on: {producto}")
+summa = numero_1 + numero_2 + numero_3
+tulos = numero_1 * numero_2 * numero_3
+keskiarvo = summa / 3
+print(f"Keskiarvo on: {keskiarvo} \nSumma on {summa} \nTulos on: {tulos}")
