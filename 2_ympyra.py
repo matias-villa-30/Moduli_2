3270fde9e4f7d90dcf32ef9769d9e29fa235dcb1
matias-villa-30 <matias.villalemes@metropolia.fi>
2024-08-21 09:24:12 +0300

    Update 2_ympyra.py

--- 2_ympyra.py
+++ 2_ympyra.py
@@ -2,6 +2,6 @@ import math
 
 pi = math.pi
 sade = int(input("Kirjoita ympyrän säde (cm): "))
-pint_ala = (circulo*circulo) * pi
+pinta_ala = (sade*sade) * pi
 
 print(f"Ympyrän pinta-ala on: {pinta_ala:.2f} cm^2")
